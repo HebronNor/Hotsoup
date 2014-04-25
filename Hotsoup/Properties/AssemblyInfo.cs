320f062d99bd35a9160db134dcca30a03102739d
HebronNor <hebron@tilhol.net>
2014-04-25 12:59:05 +0200

    Bumped version to 1.0

--- Hotsoup/Properties/AssemblyInfo.cs
+++ Hotsoup/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Home Theater Startup")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Start-up menu for home theater systems")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Hotsoup")]
-[assembly: AssemblyCopyright("Copyright © Thomas Jensen 2013")]
+[assembly: AssemblyCopyright("Copyright © Thomas Jensen 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
